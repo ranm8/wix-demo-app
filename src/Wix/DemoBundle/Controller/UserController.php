38f067c1984a45098f92522068e072c53a7c27d0
Ran Mizrahi <ranm@codeoasis.com>
2013-03-06 18:00:36 +0200

    first commit

---
+++ src/Wix/DemoBundle/Controller/UserController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Wix\DemoBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Wix\FrameworkBundle\Controller\WixController;
+use Symfony\Component\HttpFoundation\Response;
+use Wix\FrameworkBundle\Document\User;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
+use Wix\FrameworkBundle\Exception\MissingParametersException;
+use Wix\FrameworkBundle\Document\UserInterface;
+use Wix\FrameworkBundle\Configuration\Permission;
+
+/**
+ * @Route("/user")
+ */
+class UserController extends WixController
+{
+    /**
+     * @Route("/")
+     * @Method({"GET"})
+     * @Template
+     */
+    public function getUserAction()
+    {
+        /** @var User $user */
+        $user = $this->getUserDocument();
+
+        return $this->jsonResponse($user);
+    }
+
+    /**
+     * @Route("/")
+     * @Method({"POST"})
+     * @Permission({"OWNER"})
+     */
+    public function updateUserAction() {
+        $data = json_decode($this->getRequest()->getContent());
+
+        if ($data === null) {
+            throw new MissingParametersException('POST data was not given');
+        }
+
+        /** @var UserInterface $user */
+        $user = $this->getUserDocument();
+        $user = $this->updateUserDoc($user);
+
+        return $this->jsonResponse($user);
+    }
+
+    /**
+     * Serializes the object and returns JSON response
+     * @param $object
+     * @return Response
+     */
+    protected function jsonResponse($object)
+    {
+        $response = new Response($this->getSerializer()->serialize($object, 'json'));
+        $response->headers->set('Content-type', 'application/json');
+
+        return $response;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getDocumentType()
+    {
+        return 'WixFrameworkBundle:User';
+    }
+
+    /**
+     * Returns GetSetMethod JSON serializer object
+     * @return Serializer
+     */
+    protected function getSerializer()
+    {
+        $serializer = new Serializer(array(new GetSetMethodNormalizer()), array(new JsonEncoder()));
+
+        return $serializer;
+    }
+
+}
---
+++ src/Wix/DemoBundle/WixDemoBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Wix\DemoBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WixDemoBundle extends Bundle
+{
+}
